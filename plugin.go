08717cf5e2d5d641b40827c2f6aabd4cf4694b52
mwas <francismwangi152@gmail.com>
2018-07-17 10:58:22 +0300

    dockerized

--- plugin.go
+++ plugin.go
@@ -1,12 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/kadende/kadende-interfaces/pkg/types"
+	"github.com/kadende/kadende-interfaces/spi/instance"
+)
 
-type greeting string
+type plugin string
 
-func (g greeting) Greet() {
-	fmt.Println("Hello Universe")
+
+func (f plugin) Validate(req *types.Any) error {
+	return nil
 }
 
-// exported as symbol named "Greeter"
-var Plugin greeting
+func (f plugin) Provision(spec instance.Spec) (*instance.ID, error) {
+	fieldId := instance.ID("aldjlajla")
+	return &fieldId, nil
+}
+
+func (f plugin) Label(id instance.ID, labels map[string]string) (err error) {
+	return nil
+}
+
+func (f plugin) Destroy(id instance.ID, context instance.Context) (err error) {
+	return nil
+}
+
+func (f plugin) DescribeInstances(labels map[string]string, properties bool) (instances []instance.Description, err error)  {
+	return []instance.Description{}, nil
+}
+
+var Plugin plugin
